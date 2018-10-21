e94322b088ce253c13a50663ccb573a7984ef28a
IonutGrad <ionutgrad101@gmail.com>
2018-10-22 01:30:08 +0300

    lab 1 done

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        final int noAccounts =100;
-        final int noThreads = 200;
-        final int noOperationsForThread = 50;
+        final int noAccounts =1500;
+        final int noThreads = 300;
+        final int noOperationsForThread = 20;
 
         Transfers transfers = new Transfers(noAccounts, noThreads, noOperationsForThread);
 
