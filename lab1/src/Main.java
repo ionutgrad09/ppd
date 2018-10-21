3b79e458262b4182e6795fcfd6e8c0a4ed29f3f0
IonutGrad <ionutgrad101@gmail.com>
2018-10-21 22:12:22 +0300

    lab 1 done

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,15 +1,18 @@
 
-
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        final int noAccounts = 10;
-        final int noThreads = 100;
-        final int noOperationsForThread = 100;
+        final int noAccounts =100;
+        final int noThreads = 200;
+        final int noOperationsForThread = 50;
 
         Transfers transfers = new Transfers(noAccounts, noThreads, noOperationsForThread);
+
+        long startTime = System.nanoTime();
         transfers.startOperations();
+        long endTime = System.nanoTime();
+        System.out.println("Took " + (endTime - startTime) + " s");
+
     }
 }
 
